0632e7c992c48caf8e5d026e33d8f16c5f9523a2
Carlos Mancilla <c.mancillagonz@gmail.com>
2018-05-06 04:42:40 -0300

    penoso intento de login

--- test2/urls.py
+++ test2/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from test2 import views
 
 urlpatterns = [
-    path('asdasdasasdhasjdhj/', views.index, name="iniciiio"),
-    path('index/', views.index_2, name="inicio_2"),
-
+    #path('asdasdasasdhasjdhj/', views.index, name="iniciiio"),
+    #path('index/', views.index_2, name="inicio_2"),
+    path('vista1', views.index, name='index'),
+    path('login', views.login, name='login'),
 ]
